3464a66a93d97c83fd0e2cf8233c62f30f1ea2a4
Lucuferus <50552394+Lucuferus@users.noreply.github.com>
2025-09-07 19:01:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'calcul-filetage-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './css/style.css',
+  './js/script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 // Installation du SW et mise en cache des fichiers
@@ -34,6 +34,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
+      .catch(() => caches.match('./index.html'))
   );
 });
